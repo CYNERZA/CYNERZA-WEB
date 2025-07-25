84ed09322c5f59b73d33d8404b55767766debcd2
hardipsolanki22 <hardipsolanki2224@gmail.com>
2025-07-25 21:56:15 +0530

    add axios instance as a helper

---
+++ src/helpers/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// const baseURL = "http://localhost:8080/api/v1"
+const baseURL = "http:///localhost:3000/api/v1"
+
+const axiosInstance = axios.create({
+        baseURL,
+        withCredentials: true,
+    })
+
+export {axiosInstance}
