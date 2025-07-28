edeba54b1d2a0542b35659553738bd4aaa7fe02e
hardipsolanki22 <hardipsolanki2224@gmail.com>
2025-07-28 16:28:13 +0530

    add new edit and delete blog post slice

--- src/featured/blog/blogSlice.js
+++ src/featured/blog/blogSlice.js
@@ -16,7 +16,7 @@ export const fetchBlogPosts = createAsyncThunk(
             const response = await axiosInstance.get('/blogs/');
             return response.data;
         } catch (error) {
-            console.error('Failed to fetch blog posts:', error);
+            console.error('Failed to fetch blog posts:', error.message);
             throw error;
         }
     }
@@ -24,40 +24,68 @@ export const fetchBlogPosts = createAsyncThunk(
 
 export const createBlogPost = createAsyncThunk(
     'blog/createBlogPost',
-    async (newPost) => {
+    async (blogPost) => {
         try {
-            console.log({newPost})
-            const response = await axiosInstance.post('/blogs/', newPost);
+            const response = await axiosInstance.post('/blogs/', blogPost);
             return response.data;
         } catch (error) {
-            console.error('Failed to create blog post:', error);
+            console.error('Failed to create blog post:', error.message);
             throw error;
         }
     }
 );
 
+export const updateBlogPost = createAsyncThunk(
+    "blog/updateBlogPost",
+    async ({ blogId, blogPost }) => {
+        try {
+            const response = await axiosInstance.patch(`/blogs/${blogId}`, blogPost)
+            return response.data
+        } catch (error) {
+            console.log("Failed to update blog post: ", error.message)
+            throw error
+        }
+    }
+)
+
+export const deleteBlogPost = createAsyncThunk(
+    "blog/deleteBlogPost",
+    async (blogId) => {
+        try {
+            const response = await axiosInstance.delete(`/blogs/${blogId}`)
+            return response.data
+        } catch (error) {
+            console.log("Failed to delete blog post: ", error.message)
+            throw error
+        }
+    }
+)
 
 const blogSlice = createSlice({
     name: 'blog',
     initialState,
-    reducers: {},   
+    reducers: {},
     extraReducers: (builder) => {
         builder
+            // fetch all blogs 
             .addCase(fetchBlogPosts.pending, (state) => {
                 state.loading = true;
-                state.errorMessage = '';
-                state.successMessage = '';
+                state.errorMessage = "";
+                state.successMessage = "";
             })
             .addCase(fetchBlogPosts.fulfilled, (state, action) => {
                 state.loading = false;
                 state.blogPosts = action.payload.data;
                 state.successMessage = action.payload.message;
-                state.errorMessage = '';
+                state.errorMessage = "";
             })
             .addCase(fetchBlogPosts.rejected, (state, action) => {
                 state.loading = false;
                 state.errorMessage = action.error.message;
+                state.successMessage = ""
             })
+
+            // create blog 
             .addCase(createBlogPost.pending, (state) => {
                 state.loading = true;
                 state.errorMessage = '';
@@ -72,7 +100,46 @@ const blogSlice = createSlice({
             .addCase(createBlogPost.rejected, (state, action) => {
                 state.loading = false;
                 state.errorMessage = action.error.message;
-            });
+            })
+
+            // update blog post
+            .addCase(updateBlogPost.pending, (state) => {
+                state.loading = true
+                state.successMessage = ""
+                state.errorMessage = ""
+            })
+            .addCase(updateBlogPost.fulfilled, (state, action) => {
+                state.loading = false
+                const index = state.blogPosts.findIndex(blog => blog._id === action.payload.data._id)
+                if (index !== -1) {
+                    state.blogPosts[index] = action.payload.data
+                }
+                state.successMessage = action.payload.message
+                state.errorMessage = ""
+
+            })
+            .addCase(updateBlogPost.rejected, (state, action) => {
+                state.loading = false
+                state.errorMessage = action.error.message
+                state.successMessage = ""
+            })
+
+            // delete blog post
+            .addCase(deleteBlogPost.pending, (state, action) => {
+                state.loading = true
+            })
+            .addCase(deleteBlogPost.fulfilled, (state, action) => {
+                state.loading = false
+                const index = state.blogPosts.findIndex(blog => blog._id === action.payload._id)
+                if (index !== -1) {
+                    state.blogPosts.splice(index, 1)
+                }
+            })
+            .addCase(deleteBlogPost.rejected, (state, action) => {
+                state.loading = false
+
+            })
+
     },
 
 });
