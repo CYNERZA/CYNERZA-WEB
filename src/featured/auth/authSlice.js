6244bd50b0f136913ce20fa7b32bb8e3598287a9
hardipsolanki22 <hardipsolanki2224@gmail.com>
2025-07-31 17:16:32 +0530

    add and fixes some isuue in auth slice

--- src/featured/auth/authSlice.js
+++ src/featured/auth/authSlice.js
@@ -3,26 +3,27 @@ import { axiosInstance } from "../../helpers/axiosInstance";
 
 const initialState = {
     loading: false,
-    errorMessage: "",
-    successMessage: "",
     status: false,
     userData: null,
 }
 
-export const login = createAsyncThunk("/auth/login", async (userData) => {
-    try {
-        const response = await axiosInstance.post("/users/login", userData);
-        return response.data;
-    } catch (error) {
-        console.error("Login failed:", error);
-        throw error;
+export const login = createAsyncThunk(
+    "/auth/login",
+    async (userData, { rejectWithValue }) => {
+        try {
+            const response = await axiosInstance.post("/users/login", userData);
+            return response.data;
+        } catch (error) {
+            console.error("Login failed:", error);
+            return rejectWithValue(error.response?.data?.message || "Login failed");
+        }
     }
-});
+);
 
 export const logout = createAsyncThunk("/users/auth/logout", async () => {
     try {
         const response = await axiosInstance.post("/users/logout");
-        return {};
+        return response.data;
     } catch (error) {
         console.error("Logout failed:", error);
         throw error;
@@ -40,11 +41,12 @@ export const getCurrentUser = createAsyncThunk("/auth/currentUser", async () => 
 });
 
 const authSlice = createSlice({
-    name: "auth",  
+    name: "auth",
     initialState,
     reducers: {},
     extraReducers: (builder) => {
         builder
+            // login
             .addCase(login.pending, (state) => {
                 state.loading = true;
                 state.errorMessage = "";
@@ -62,6 +64,8 @@ const authSlice = createSlice({
                 state.errorMessage = action.error.message
                 state.successMessage = "";
             })
+
+            // logout
             .addCase(logout.pending, (state) => {
                 state.loading = true;
                 state.successMessage = "";
@@ -78,17 +82,19 @@ const authSlice = createSlice({
                 state.successMessage = "";
                 state.errorMessage = action.error.message;
             })
+
+            // current user
             .addCase(getCurrentUser.pending, (state) => {
                 state.loading = true;
                 state.successMessage = "";
                 state.errorMessage = "";
-            
+
             })
             .addCase(getCurrentUser.fulfilled, (state, action) => {
                 state.loading = false;
                 state.userData = action.payload;
                 state.status = true;
-                
+
             })
             .addCase(getCurrentUser.rejected, (state) => {
                 state.loading = false;
